e40c59f8507e0e584c5c29ab0b5e8688fee3546c
Sahil <Sahil2003@gmail.com>
2023-12-18 18:15:49 +0530

    About Section Done

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,38 +1,26 @@
-import React, { Suspense, useEffect, useState } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
-import CanvasLoader from '../Loader';
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const [computer, setComputer] = useState(null);
-
-  useEffect(() => {
-    const loadComputer = async () => {
-      const gltf = await useGLTF('./desktop_pc/scene.gltf');
-      setComputer(gltf);
-    };
-
-    loadComputer();
-  }, []);
-
-  if (!computer) {
-    return null; // or a loading indicator
-  }
+  const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
-      <pointLight intensity={1} />
+      <hemisphereLight intensity={0.15} groundColor='black' />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
         penumbra={1}
         intensity={1}
         castShadow
-        shadow-mapSize-width={1024}
-
+        shadow-mapSize={1024}
       />
-      <primitive object={computer.scene}
+      <pointLight intensity={1} />
+      <primitive
+        object={computer.scene}
         scale={isMobile ? 0.7 : 0.75}
         position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
@@ -45,25 +33,31 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    const mediaQuery = window.matchMedia('(max-width: 500px)');
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the `isMobile` state variable
     setIsMobile(mediaQuery.matches);
 
-    const handleMediaQueryChange = (e) => {
-      setIsMobile(e.matches);
-    }
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
 
-    mediaQuery.addListener(handleMediaQueryChange);
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
 
+    // Remove the listener when the component is unmounted
     return () => {
-      mediaQuery.removeListener(handleMediaQueryChange);
-    }
-
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
   }, []);
 
   return (
     <Canvas
-      frameloop="demand"
+      frameloop='demand'
       shadows
+      dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
@@ -75,6 +69,7 @@ const ComputersCanvas = () => {
         />
         <Computers isMobile={isMobile} />
       </Suspense>
+
       <Preload all />
     </Canvas>
   );
